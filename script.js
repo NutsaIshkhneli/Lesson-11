fde47167547593adfb0773805af65ec8724b76ca
NutsaIshkhneli <nutsa.ishkhneli@gmail.com>
2021-03-05 13:22:25 +0400

    6

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// 1
+
 let cars = ["audi","bmw", "lexus", "volkswagen", " porsche"];
 
 
@@ -5,6 +8,8 @@ for ( let i =2; i<5; i++ ) {
     console.log(cars [i],i);
 }
 
+
+// 2
 
 let names = ["irakli","elene", "nutsa", "ia", " nia", " salome" ];
 
@@ -15,3 +20,30 @@ for (let i=0; i<names.length; i++) {
 
 
 
+
+
+// 4
+
+let user = [ 'name', 'age', 25, 50, 'surname', [100,500] ];
+
+console.log (user[5][1]);
+
+// 5
+
+var person = {
+    firstname: "john",
+    lastname: "doe",
+    age: 50,
+    eyecolor: "blue"
+  };
+
+  console.log(person.eyecolor);
+
+// 6
+
+let numbers = [3, 6 , 97, 30]
+
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
